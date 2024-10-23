262420b9a171c77aaed6adfd63f101c4283abc86
jonorl <39122230+jonorl@users.noreply.github.com>
2024-10-23 20:46:11 +0300

    Added comments on index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,17 @@
+// Import other js modules and main CSS
+
 import "../css/style.css";
 import { home } from "./home.js"
 import { menu } from "./menu.js"
 import { about } from "./about.js"
 
+// Button selectors
 const homeButton = document.querySelector("#home")
 const menuButton = document.querySelector("#menu")
 const aboutButton = document.querySelector("#about")
 
+
+// Event Listeners
 homeButton.addEventListener("click", () => {
     home();
 });
@@ -19,4 +24,5 @@ aboutButton.addEventListener("click", () => {
     about();
 });
 
+// Console log as requested
 console.log("hello restaurant")
